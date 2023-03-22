68b296c20c2ef02ec51f656390ed6f815c1155c5
maldassin <maendassin@gmail.com>
2023-03-22 11:54:24 +0300

    Revert "set up tailwind css"
    This reverts commit 98fd2b65953b86e4acb8257be4d357f029456059.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import './index.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [role, setRole] = useState();
-  const showEmployees = true;
   return (
-    <div className='App bg-red-300'>
-      {showEmployees ?
-        <>
-          <input type='text' onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value);
-          }} />
-          <Employee name='Maen' role='Intern' />
-          <Employee name='Abby' role={role}/>
-          <Employee name='John' role={role}/>
-        </>
-        :
-        <p>Access denied.</p>
-      }
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello!
+        </p>
+      </header>
     </div>
   );
 }
