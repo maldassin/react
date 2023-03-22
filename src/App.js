93dada8fa6d3da41615685d1b11142f0d9cfa0bf
maldassin <maendassin@gmail.com>
2023-03-22 12:12:52 +0300

    Returning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import './index.css';
+import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState();
+  const showEmployees = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello!
-        </p>
-      </header>
+    <div className='App flex h-screen flex-col justify-center items-center'>
+      {showEmployees ?
+        <>
+          <input className='p-2' type='text' placeholder='Search' onChange={(e) => {
+            setRole(e.target.value);
+          }} />
+          <Employee name='Maen' role='Intern' />
+          <Employee name='Abby' role={role}/>
+          <Employee name='John' role={role}/>
+        </>
+        :
+        <p>Access denied.</p>
+      }
     </div>
   );
 }
